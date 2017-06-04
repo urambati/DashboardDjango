7c476594d6463b5230945c0e1bd858f07cc271a5
ukiras123 <ukiras@gmail.com>
2017-06-03 19:37:55 -0700

    adding music page

--- music/views.py
+++ music/views.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("<h1>This is a music App</h1>")
